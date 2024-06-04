66510c9c533e8a0006c85707d82cce547a5ebfcf
Lucas Lima <130606633+lucas-limabr@users.noreply.github.com>
2024-06-04 19:15:17 -0300

    alterando visibilidade e definindo get/set

--- debug/testeConta.php
+++ debug/testeConta.php
@@ -6,6 +6,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $c1 = new ContaBancaria();
 
-var_dump($c1);
-var_dump($c1->exibirDados());
-echo $c1->nome_titular;
+$c1->setBanco('Banco do Brasil');
+$c1->setNome('Lucas');
+$c1->setAgencia('3486');
+$c1->setConta('17854-2');
+$c1->setSaldo('100.8');
+
+print_r($c1->exibirDados());
+echo $c1->getBanco();
--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -6,11 +6,11 @@ declare(strict_types= 1);
 namespace App;
 
 class ContaBancaria{
-    public string $banco = "Inter";
-    public string $nome_titular = "Lucas";
-    public string $num_agencia = "3456";
-    public string $num_conta = "17893-5";
-    public float $saldo = 0;
+    private string $banco;
+    private string $nome_titular;
+    private string $num_agencia;
+    private string $num_conta;
+    private float $saldo;
 
     public function exibirDados():array{
         return [
@@ -20,5 +20,38 @@ class ContaBancaria{
             "Conta"=> $this->num_conta,
             "Saldo"=> $this->saldo
         ];
+    }
+
+    //getters and setters
+    public function getBanco():string{
+        return $this->banco;
+    }
+    public function getNome():string{
+        return $this->nome_titular;
+    }
+    public function getAgencia():string{
+        return $this->num_agencia;
+    }
+    public function getConta():string{
+        return $this->num_conta;
+    }
+    public function getSaldo():float{
+        return $this->saldo;
+    }
+
+    public function setBanco(string $banco):void{
+       $this->banco = $banco;
+    }
+    public function setNome(string $nome):void{
+        $this->nome_titular = $nome;
+    }
+    public function setAgencia(string $agencia):void{
+        $this->num_agencia = $agencia;
+    }
+    public function setConta(string $conta):void{
+        $this->num_conta = $conta;
+    }
+    public function setSaldo(float $saldo):void{
+       $this->saldo = $saldo ;
     }
 }
